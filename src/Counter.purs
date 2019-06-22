920b07fe99499d8df6bc900b651a8d0afe33336b
Jason Kim <actcrazy@gmail.com>
2019-06-23 07:36:55 +0900

    feat: counter, app 구현

---
+++ src/Counter.purs
@@ -0,0 +1,63 @@
+module Counter
+  ( mkApp
+  ) where
+
+import Prelude
+
+import React.Basic.DOM as R
+import React.Basic.Events (EventHandler, handler_)
+import React.Basic.Hooks (CreateComponent, Hook, UseState, component, element, fragment, useState, (/\))
+import React.Basic.Hooks as React
+
+type Counter =
+  { value :: Int
+  , increase :: EventHandler
+  , decrease :: EventHandler
+  , reset :: EventHandler
+  }
+
+useCounter :: Int -> Hook (UseState Int) Counter
+useCounter initialCount = React.do
+  count /\ setCounter <- useState initialCount
+  pure
+    { value: count
+    , increase: handler_ $ setCounter (_ + 1)
+    , decrease: handler_ $ setCounter (_ - 1)
+    , reset: handler_ $ setCounter (\_ -> initialCount)
+    }
+
+mkCounter :: CreateComponent { initialCount :: Int }
+mkCounter = do
+  component "Counter" \{ initialCount } -> React.do
+    counter <- useCounter initialCount
+
+    pure $ fragment
+      [ R.text $ "Count: " <> show counter.value
+      , R.br {}
+      , R.br {}
+      , R.button
+          { onClick: counter.reset
+          , children: [ R.text "Reset" ]
+          }
+      , R.button
+          { onClick: counter.increase
+          , children: [ R.text "+" ]
+          }
+      , R.button
+          { onClick: counter.decrease
+          , children: [ R.text "-" ]
+          }
+      ]
+
+mkApp :: CreateComponent {}
+mkApp = do
+  counter <- mkCounter
+
+  component "App" \_ ->
+    pure $ R.div
+      { className: "App"
+      , children:
+          [ R.h1_ [R.text "React Hooks Example"]
+          , element counter { initialCount: 1 }
+          ]
+    }
--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,26 @@
 module Main where
 
 import Prelude
+
+import Counter (mkApp)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
+import Effect.Exception (throw)
+import React.Basic (element)
+import React.Basic.DOM (render)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  log "Rendering counter app."
+
+  container <- (toNonElementParentNode <$> (window >>= document)) >>= getElementById "container"
+  case container of
+    Nothing -> throw "Container element not found."
+    Just c -> do
+      app <- mkApp
+      render (element app {}) c
