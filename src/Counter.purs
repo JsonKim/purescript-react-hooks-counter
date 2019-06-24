45f7e1400009b6f0a1e99a73c88d41bdcce6ca90
Jason Kim <actcrazy@gmail.com>
2019-06-25 01:42:55 +0900

    refactor: 카운터에 useCallback 적용

--- src/Counter.purs
+++ src/Counter.purs
@@ -4,26 +4,39 @@ module Counter
 
 import Prelude
 
+import Effect (Effect)
 import React.Basic.DOM as R
 import React.Basic.Events (EventHandler, handler_)
-import React.Basic.Hooks (CreateComponent, Hook, UseState, component, element, fragment, useState, (/\))
+import React.Basic.Hooks (CreateComponent, Hook, UnsafeReference(..), UseCallback, UseState, component, element, fragment, useCallback, useState, (/\))
 import React.Basic.Hooks as React
+
+type UpdateState a = a -> a
+type StateDispatch a = UpdateState a -> Effect Unit
+type HookCallback a b = Hook (UseCallback (UnsafeReference (StateDispatch a)) (Effect Unit)) b
 
 type Counter =
   { value :: Int
-  , increase :: EventHandler
-  , decrease :: EventHandler
-  , reset :: EventHandler
+  , increase :: HookCallback Int EventHandler
+  , decrease :: HookCallback Int EventHandler
+  , reset :: HookCallback Int EventHandler
   }
+
+counterCallback :: ∀ a. StateDispatch a → UpdateState a → HookCallback a (Effect Unit)
+counterCallback s c = useCallback (UnsafeReference s) (s c)
+
+handlerCallback :: ∀ a. StateDispatch a → UpdateState a → HookCallback a EventHandler
+handlerCallback s c = React.do
+  u <- counterCallback s c
+  pure $ handler_ u
 
 useCounter :: Int -> Hook (UseState Int) Counter
 useCounter initialCount = React.do
   count /\ setCounter <- useState initialCount
   pure
     { value: count
-    , increase: handler_ $ setCounter (_ + 1)
-    , decrease: handler_ $ setCounter (_ - 1)
-    , reset: handler_ $ setCounter (\_ -> initialCount)
+    , increase: handlerCallback setCounter (_ + 1)
+    , decrease: handlerCallback setCounter (_ - 1)
+    , reset: handlerCallback setCounter (const initialCount)
     }
 
 mkCounter :: CreateComponent { initialCount :: Int }
@@ -31,20 +44,24 @@ mkCounter = do
   component "Counter" \{ initialCount } -> React.do
     counter <- useCounter initialCount
 
+    reset <- counter.reset
+    increase <- counter.increase
+    decrease <- counter.decrease
+
     pure $ fragment
       [ R.text $ "Count: " <> show counter.value
       , R.br {}
       , R.br {}
       , R.button
-          { onClick: counter.reset
+          { onClick: reset 
           , children: [ R.text "Reset" ]
           }
       , R.button
-          { onClick: counter.increase
+          { onClick: increase
           , children: [ R.text "+" ]
           }
       , R.button
-          { onClick: counter.decrease
+          { onClick: decrease
           , children: [ R.text "-" ]
           }
       ]
